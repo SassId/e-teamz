2a406e85c1476316ec7c1af2e64623ac588ea50f
BakiBacci <isaakibat@gmail.com>
2024-03-20 13:48:10 +0100

    13:48

---
+++ src/javascript/service/local-auth-service.js
@@ -0,0 +1,11 @@
+export class LocalAuthService {
+    static async login (data){
+        try{
+            const result = await fetch('/src/data')
+
+        }
+        catch(e){
+            console.log(e);
+        }
+    }
+}
